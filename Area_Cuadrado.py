656c5c8ed36a670b5cd078d7b6e1bdd9637d1e35
Anthony140405 <jhonarevalo1404@gmail.com>
2024-06-30 21:01:30 -0500

    Delete Area_Cuadrado.py

--- Area_Cuadrado.py
+++
@@ -1,8 +0,0 @@
-# Solicitar al usuario que ingrese la medida del lado del cuadrado
-lado = float(input("Ingresa la medida del lado del cuadrado: "))
-
-# Calcular el área del cuadrado
-area = lado * lado
-
-# Mostrar el resultado
-print(f"El área del cuadrado con lado {lado} es: {area}")
